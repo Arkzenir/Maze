9fb94302c325df84be218748ff68dfaf3ae8d986
Alper Sarı <alper.sari.work@gmail.com>
2023-12-09 13:29:31 +0300

    Add DOTween, add new prefabs for tilemap, start on obstacles

---
+++ Assets/Scripts/Obstacles/ObstacleBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBase : MonoBehaviour
+{
+    [SerializeField] private Transform obstacleTransform;
+    [SerializeField] private float startValue;
+    [SerializeField] private float endValue;
+    [SerializeField] private float firstHalfDuration;
+    [SerializeField] private float secondHalfDuration;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
